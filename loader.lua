9b5c97aace30b842bc526d6fea0adf4fb28a4354
Patooh <53504041+Patooh777@users.noreply.github.com>
2023-01-27 21:53:47 -0300

    Update and rename README.md to loader.lua

--- loader.lua
+++ loader.lua
@@ -1,11 +1,8 @@
-local SSID = 5055081323
-local AEVO = 10723695195
+local S&G = 11767933308
 local GameID = game.PlaceId
 
-if (GameID == SSID) then
+if (GameID == S&G) then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Patooh777/Swordman-Simulator/SunsetXHub/README.md"))()
-elseif (GameID == AEVO) then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Patooh777/Anime-Evolution-Sim-/SunsetXHub/Source"))()
 else
     print("invalid Place")
     game.Players.LocalPlayer:Kick("Ur script dont support this shit ass game")
